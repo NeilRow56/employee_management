060eb722fa5e3e089dddedbd640c1c3413febc22
Neil <neilrowland56@yahoo.com>
2022-10-25 14:19:48 +0100

    Updated form so that it can be shown, or not shown

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from "next/head"
+import { useState } from 'react'
 import { BiUserPlus } from "react-icons/bi";
 import Form from "../components/Form";
 import Table from "../components/Table";
 
 
 const Home = () => {
+  const [ visible, setVisible ] = useState( false )
+
+  const handler = () => {
+    setVisible( visible ? false : true )
+  }
+
   return (
     
       <section>
@@ -15,14 +22,15 @@ const Home = () => {
         <h1 className="text-xl md:text-5xl text-center py-10 font-bold" >Employee Management</h1>
         <div className="container mx-auto flex justify-between py-5 border-b">
           <div className="left flex-gap-3">
-            <button className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:border-2 hover:text-gray-800 flex items-center  ">Add Employee <span className=" px-1 items-center"><BiUserPlus className="h-6 w-6" /></span>  </button>
+            <button onClick={handler} className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:border-2 hover:text-gray-800 flex items-center  ">Add Employee <span className=" px-1 items-center"><BiUserPlus className="h-6 w-6" /></span>  </button>
           </div>   
 
         </div> 
 
         {/* Collapsible Form */}
         <div className="container mx-auto">
-           <Form />
+          { visible ? <Form />: <></> }
+           
         </div>
          
         {/* Table */}
